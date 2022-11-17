d5dd74331778e625d394915265a22ccaf3cb0ccc
Fedee Gomez drees <abcrojo5@gmail.com>
2022-11-17 18:34:12 -0300

    Add project files.

---
+++ WebApiNorthwind/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiNorthwind.Models;
+
+namespace WebApiNorthwind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly NorthwindContext _context;
+
+        public CategoriesController(NorthwindContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Categorie> Get()
+        {
+            var categories = (from p in _context.Categories select p).ToList();
+            return categories;
+
+        }
+        [HttpGet("{id}")]
+        public Categorie Get(int id)
+        {
+            var categories = (from p in _context.Categories where p.CategoryId == id select p).SingleOrDefault();
+            return categories;
+
+        }
+       
+    }
+}
---
+++ WebApiNorthwind/Controllers/CustomerController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using WebApiNorthwind.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace WebApiNorthwind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerController(NorthwindContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            var customer = (from p in _context.Customers select p).ToList();
+            return customer;
+
+        }
+        [HttpGet("{id}")]
+        public Customer Get(string id) {
+            var customer = (from p in _context.Customers where p.CustomerId == id select p).SingleOrDefault();
+            return customer;
+        
+        }
+        [HttpGet("{companyName}/{contactName}")]
+        public dynamic Get(string companyName, string contactName) {
+            dynamic customer = (from p in _context.Customers where p.CompanyName == companyName && p.ContactName == contactName select new { p.CompanyName, p.ContactName });
+            return customer;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put([FromBody]Customer customer, string id)
+        {
+            if (id != customer.CustomerId)
+            {
+                BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Customer> Delete(string id)
+        {
+            var resultado = _context.Customers.FirstOrDefault(x => x.CustomerId ==id);
+            if (resultado == null)
+            { return NotFound(); }
+            _context.Customers.Remove(resultado);
+            _context.SaveChanges();
+            return resultado;
+        }
+
+
+
+    }
+}
